5bbf99cdac1d3096bf2190a89fe6963a30c006c7
Michael Lindenau <michael.lindenau@flexdrive.com>
2018-01-31 11:32:48 -0800

    upgrade to h17

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const pkg = require('./package.json');
 const register = require('./src/middleware-wrapper');
 
-// provide meta-information as expected by hapi.js
-register.attributes = { pkg };
+const name = 'hapi_status'
 
 // export register function, wrapped in a plugin object
-module.exports = { register };
+module.exports.plugin = { 
+  name,
+  register,
+  pkg
+};
